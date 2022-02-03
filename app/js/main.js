b038052615b402766a48d481ef70be9f3d1c033b
NeverMind109 <vladgladkih123@gmail.com>
2022-02-03 18:58:03 +0300

    Created:Added Reviews Section & Tabs For Functions

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,38 @@
-$(function () {
-  $(".functions__slider-tab1").slick({
-    infinite: true,
-    speed: 400,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    nextArrow:
-      '<button class="tabs-buttons tabs-next"><svg width="61" height="32" viewBox="0 0 61 32" xmlns="http://www.w3.org/2000/svg"><linearGradient id="linear-gradient"><stop offset="-7.86%" stop-color="#0AA6FE"/><stop offset="96.71%" stop-color="#00E0FF"/></linearGradient><path d="M16.5208 0L0 16L16.5208 32L18.3178 30.2597L4.86475 17.2308H61V14.7692H4.86475L18.3178 1.74031L16.5208 0Z"/></svg></button>',
-    prevArrow:
-      '<button class="tabs-buttons tabs-prev"><svg width="61" height="32" viewBox="0 0 61 32" xmlns="http://www.w3.org/2000/svg"><linearGradient id="linear-gradient"><stop offset="-7.86%" stop-color="#0AA6FE"/><stop offset="96.71%" stop-color="#00E0FF"/></linearGradient><path d="M16.5208 0L0 16L16.5208 32L18.3178 30.2597L4.86475 17.2308H61V14.7692H4.86475L18.3178 1.74031L16.5208 0Z"/></svg></button>',
-  });
+const firstTab = new Swiper(".functions__slider-tab-1", {
+  loop: false,
+  navigation: {
+    nextEl: ".tab-1-next",
+    prevEl: ".tab-1-prev",
+  },
+});
+const secondTab = new Swiper(".functions__slider-tab-2", {
+  loop: false,
+  navigation: {
+    nextEl: ".tab-2-next",
+    prevEl: ".tab-2-prev",
+  },
+});
+const thirdTab = new Swiper(".functions__slider-tab-3", {
+  loop: false,
+  navigation: {
+    nextEl: ".tab-3-next",
+    prevEl: ".tab-3-prev",
+  },
+});
+
+$(".functions__tab").on("click", function (e) {
+  e.preventDefault();
+  $(".functions__tab").removeClass("functions__tab--active");
+  $(this).addClass("functions__tab--active");
+
+  $(".functions__content").removeClass("functions__content--active");
+  $($(this).attr("href")).addClass("functions__content--active");
+});
+
+const reviews = new Swiper(".reviews__slider", {
+  loop: true,
+  navigation: {
+    nextEl: ".reviews-next",
+    prevEl: ".reviews-prev",
+  },
 });
