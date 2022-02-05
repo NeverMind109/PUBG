2a0ff68ae79e0dfaf8fd4f3f5e728326bcbda6a5
NeverMind109 <vladgladkih123@gmail.com>
2022-02-05 18:22:39 +0300

    Created: Added Smooth Link Transition & fixes

--- app/js/main.js
+++ app/js/main.js
@@ -59,4 +59,17 @@ $(function () {
     .addClass("accordion--active")
     .next()
     .slideToggle();
+
+  $("a.scroll-to").on("click", function (e) {
+    e.preventDefault();
+    var anchor = $(this).attr("href");
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $(anchor).offset().top - 60,
+        },
+        800
+      );
+  });
 });
